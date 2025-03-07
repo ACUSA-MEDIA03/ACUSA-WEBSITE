7e1fde907c0384a8bc251f5772b3c8c6215c5a30
ifeoluwa02 <olawooreifeoluea2004@gmail.com>
2025-03-07 11:39:18 +0100

    gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage"
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
 import AboutPage from "./pages/AboutPage";
 import GalleryPage from "./pages/GalleryPage"
-=======
-=======
->>>>>>> Stashed changes
-import GalleryPage from "./pages/GalleryPage"
 
 
->>>>>>> Stashed changes
+
 
 
 function App() {
@@ -19,20 +14,18 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage/>} />
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
           <Route path="/about" element={<AboutPage />}/>
           <Route path="/gallery" element={<GalleryPage/>} />
           
 
-=======
+
+          <Route path="/gallery" element={<GalleryPage/>} />
+
+
           <Route path="/gallery" element={<GalleryPage/>} />
           
->>>>>>> Stashed changes
-=======
-          <Route path="/gallery" element={<GalleryPage/>} />
-          
->>>>>>> Stashed changes
+
         </Routes>
       </Router>
     </>
