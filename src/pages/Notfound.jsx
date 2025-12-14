14d548b4ad82d660fc59a52fc9344e10895415a6
theNubiadev <ajaiyeobajibola@gmail.com>
2025-12-14 17:32:34 +0100

    feat: added the notfound page

---
+++ src/pages/Notfound.jsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+    return (
+        <>
+            <div>
+                Not Found
+
+                <h2>Want to drive the ACUSA ark?</h2>
+            </div>
+        </>
+    )
+}
